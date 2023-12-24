9480569f155b8653cc15a1a4e9aa8e2c79a808c1
kap86 <kaplin86gamer@gmail.com>
2023-12-24 01:26:31 -0500

    initial commit (idk why it bugged the first time)

---
+++ src/main/java/com/morselsmarsh/theprotocol/TheProtocolClient.java
@@ -0,0 +1,11 @@
+package com.morselsmarsh.theprotocol;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheProtocolClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
